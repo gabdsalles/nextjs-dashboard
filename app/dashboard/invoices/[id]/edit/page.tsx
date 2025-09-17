fa8ac7c4247cb4ac384539a86b7da7bdbefa39e5
gabdsalles <gabdsalles@gmail.com>
2025-09-16 23:38:06 -0300

    correcting build issues

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -8,8 +8,8 @@ export const metadata: Metadata = {
   title: 'Edit Invoice',
 };
  
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = params.id;
+export default async function Page(props: { params: Promise<{ id: string }>; }) {
+  const id = (await props.params).id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
